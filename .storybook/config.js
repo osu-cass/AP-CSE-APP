e1362084117ebabccf44fa2f9c1a192e8d771510
Graham Barber <grahammbarber@gmail.com>
2018-07-30 15:27:57 -0700

    build(storybook): configure addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,10 @@
 import { configure } from '@storybook/react';
+import { addDecorator } from '@storybook/react';
+import { withConsole } from '@storybook/addon-console';
+import { checkA11y } from '@storybook/addon-a11y';
+
+addDecorator((storyFn, context) => withConsole()(storyFn)(context))
+addDecorator(checkA11y)
 
 function loadStories() {
   const req = require.context('../src', true, /\.storybook\.tsx$/)
