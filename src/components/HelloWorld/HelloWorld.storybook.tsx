023e4e53ec79716a0eed23db5171d7b3c6cecc15
Graham Barber <grahammbarber@gmail.com>
2018-07-30 15:26:34 -0700

    test(hello-world): add centered storybook decorator

--- src/components/HelloWorld/HelloWorld.storybook.tsx
+++ src/components/HelloWorld/HelloWorld.storybook.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import centered from '@storybook/addon-centered';
 
 import { HelloWorld } from './index';
 
-storiesOf('HelloWorld component', module).add('Renders greeting', () => <HelloWorld />);
+storiesOf('HelloWorld component', module)
+  .addDecorator(centered)
+  .add('Renders greeting', () => <HelloWorld />);
