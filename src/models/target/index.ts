f4df3dd7b2f46663fe2e709151a9d7220dffffcb
Dan Van Horn <dan.vanhorn17@gmail.com>
2018-09-05 11:28:19 -0700

    feat(data model): adds data model types to the app

---
+++ src/models/target/index.ts
@@ -0,0 +1,30 @@
+// tslint:disable:no-reserved-keywords
+
+export interface ITarget {
+  title: string;
+  shortCode: string;
+  description: string;
+  standards: string[];
+  stdDesc: string[];
+  DOK: string[];
+  DOKDesc: string[];
+  shortDOK: string[];
+  type: string;
+  clarification: string;
+  heading: string;
+  evidence: string[];
+  vocab: string;
+  tools: string;
+  stimInfo: string;
+  devNotes: string;
+  complexity: string;
+  dualText: string;
+  accessibility: string;
+  stem: string[];
+  taskDescription: string[];
+  taskModel: string[];
+  examples: string[];
+  rubrics: string[];
+  stimulus: string[];
+  shortStem: string[];
+}
