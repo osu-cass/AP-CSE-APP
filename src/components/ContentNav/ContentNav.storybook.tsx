8297906c505ced0b088d35e13c24287b71f5fe5e
Henry Peterson <peterhen@oregonstate.edu>
2018-09-06 11:01:25 -0700

    feat(content-nav): add basic active and expanded item functionality

--- src/components/ContentNav/ContentNav.storybook.tsx
+++ src/components/ContentNav/ContentNav.storybook.tsx
@@ -5,7 +5,7 @@ import centered from '@storybook/addon-centered';
 import { ContentNav, Item, SubItem } from '.';
 
 const subMock1: SubItem = { name: 'sdgasdfasdf' };
-const subMock2: SubItem = { name: 'helasdfasdfsdasdglo' };
+const subMock2: SubItem = { name: 'helasd' };
 const mock: Item[] = [{ name: 'hello' }, { name: 'another', subItems: [subMock1, subMock2] }];
 
 storiesOf('ContentNav component', module)
