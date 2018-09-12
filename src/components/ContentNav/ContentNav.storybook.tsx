34d37a4106ad998eb4f6dc464f2cd2520fa5e706
Henry Peterson <peterhen@oregonstate.edu>
2018-09-12 08:49:50 -0700

    refactor(content-nav): better organize the code, fix a warning

--- src/components/ContentNav/ContentNav.storybook.tsx
+++ src/components/ContentNav/ContentNav.storybook.tsx
@@ -2,11 +2,46 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import centered from '@storybook/addon-centered';
 
-import { ContentNav, Item, SubItem } from '.';
+import { ContentNav } from '.';
+import { ItemProps } from './Item';
+import { SubItemProps } from './SubItem';
 
-const subMock1: SubItem = { name: 'sdgasdfasdf' };
-const subMock2: SubItem = { name: 'helasd' };
-const mock: Item[] = [{ name: 'hello', subItems: [subMock1, subMock2] }, { name: 'another' }];
+const subMock1: SubItemProps = { name: 'sdgasdfasdf' };
+const subMock2: SubItemProps = { name: 'helasd' };
+const mock: ItemProps[] = [
+  {
+    name: 'Clarifications'
+  },
+  {
+    name: 'Standards'
+  },
+  {
+    name: 'Stimuli/Text Complexity'
+  },
+  {
+    name: 'Accessibility Concerns'
+  },
+  {
+    name: 'Evidence Required'
+  },
+  {
+    name: 'Task Model 1',
+    subItems: [
+      {
+        name: 'Task Descriptions'
+      },
+      {
+        name: 'Target Evidence Statement'
+      },
+      {
+        name: 'Appropriate Stems'
+      }
+    ]
+  },
+  {
+    name: 'Scoring Rules'
+  }
+];
 
 storiesOf('ContentNav component', module)
   .addDecorator(centered)
--- src/components/ContentNav/index.tsx
+++ src/components/ContentNav/index.tsx
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
 import { Colors, Styles } from '../../constants';
 import { SbNavLink } from '../SbNavLink';
-import { ChevronDown, ChevronUp } from 'react-feather';
+import { Item, ItemProps } from './Item';
+import { SubItem, SubItemProps } from './SubItem';
 
-export interface SubItem {
-  active?: boolean;
-  name: string;
-}
-
-export interface Item extends SubItem {
-  expanded?: boolean;
-  subItems?: SubItem[];
-}
-
+/**
+ * Properties for ContentNav
+ * @export
+ * @interface ContentNavProps
+ */
 export interface ContentNavProps {
-  items: Item[];
+  items: ItemProps[];
   activeElement?: string;
 }
 
+/**
+ * Statefor ContentNav
+ * @export
+ * @interface ContentNavProps
+ */
 export interface ContentNavState {
-  items: Item[];
+  items: ItemProps[];
 }
 
+/**
+ * Renders a content navigator for the target viewer.
+ * @export
+ * @class {ContentNav}
+ * @param {ContentNavProps} item
+ */
 export class ContentNav extends Component<ContentNavProps, ContentNavState> {
   constructor(props: ContentNavProps) {
     super(props);
@@ -30,8 +37,8 @@ export class ContentNav extends Component<ContentNavProps, ContentNavState> {
     };
   }
 
-  constructItems(items: Item[]): Item[] {
-    return items.map((item: Item) => {
+  constructItems(items: ItemProps[]): ItemProps[] {
+    return items.map((item: ItemProps) => {
       const isActive = item.name === this.props.activeElement;
       let newItem = { ...item, active: isActive };
       let subItemIsActive = false;
@@ -49,12 +56,12 @@ export class ContentNav extends Component<ContentNavProps, ContentNavState> {
     });
   }
 
-  subItemClicked(event: React.MouseEvent<HTMLLIElement>, name: string) {
+  subItemClicked = (event: React.MouseEvent<HTMLLIElement>, name: string) => {
     event.stopPropagation();
-    const newItems: Item[] = this.state.items.map((item: Item) => {
+    const newItems: ItemProps[] = this.state.items.map((item: ItemProps) => {
       let isItemActive = false;
       if (item.subItems) {
-        item.subItems = item.subItems.map((subItem: SubItem) => {
+        item.subItems = item.subItems.map((subItem: SubItemProps) => {
           const isActive = subItem.name === name;
           isItemActive = !isItemActive ? isActive : true;
 
@@ -68,14 +75,14 @@ export class ContentNav extends Component<ContentNavProps, ContentNavState> {
     this.setState({
       items: newItems
     });
-  }
+  };
 
-  itemClicked(name: string) {
-    const newItems: Item[] = this.state.items.map((item: Item) => {
+  itemClicked = (event: React.MouseEvent<HTMLLIElement>, name: string) => {
+    const newItems: ItemProps[] = this.state.items.map((item: ItemProps) => {
       const isActive = item.name === name;
-      let newSubItems: SubItem[] | undefined;
+      let newSubItems: SubItemProps[] | undefined;
       if (item.subItems) {
-        newSubItems = item.subItems.map((subItem: SubItem) => ({ ...subItem, active: false }));
+        newSubItems = item.subItems.map((subItem: SubItemProps) => ({ ...subItem, active: false }));
       }
 
       return { ...item, subItems: newSubItems, active: isActive };
@@ -84,11 +91,11 @@ export class ContentNav extends Component<ContentNavProps, ContentNavState> {
     this.setState({
       items: newItems
     });
-  }
+  };
 
-  expand(event: React.MouseEvent<SVGElement>, name: string) {
+  expand = (event: React.MouseEvent<SVGElement>, name: string) => {
     event.stopPropagation();
-    const newItems: Item[] = this.state.items.map((item: Item) => {
+    const newItems: ItemProps[] = this.state.items.map((item: ItemProps) => {
       const isExpanded = item.name === name ? !item.expanded : item.expanded;
 
       return { ...item, expanded: isExpanded };
@@ -97,160 +104,66 @@ export class ContentNav extends Component<ContentNavProps, ContentNavState> {
     this.setState({
       items: newItems
     });
-  }
-
-  renderItems(items: Item[]): JSX.Element[] {
-    return items.map(({ name, subItems, expanded, active }: Item) => {
-      let subs: JSX.Element | undefined;
-
-      if (subItems) {
-        subs = (
-          <ul className={`sub-list ${expanded ? 'expanded' : 'collapsed'}`}>
-            {this.renderSubItems(subItems, name)}
-            <style jsx>{`
-              ul {
-                list-style-type: none;
-                padding-left: 0;
-              }
-              .expanded {
-                height: auto;
-              }
-              :not(.expanded) {
-                height: 0;
-                overflow: hidden;
-              }
-            `}</style>
-          </ul>
-        );
-      }
-      const down = (
-        <ChevronDown
-          onClick={e => {
-            this.expand(e, name);
-          }}
-        />
-      );
-      const up = (
-        <ChevronUp
-          onClick={e => {
-            this.expand(e, name);
-          }}
-        />
-      );
-
-      return (
-        <div>
-          <li
-            className={`sub-list ${expanded && 'expanded'} `}
-            key={name}
-            onClick={() => this.itemClicked(name)}
-            role="menuitem"
-          >
-            <div className={`${active && 'active'}`}>
-              <div className="item-content">
-                {name}
-                {subs && (expanded ? up : down)}
-              </div>
-            </div>
-            {subs}
-            <style jsx>{`
-              li {
-                position: relative;
-                display: flex;
-                flex-direction: column;
-                border-style: solid;
-                border-width: 1px 0 0 0;
-                font-size: 24px;
-                font-weight: bold;
-                text-indent: 0.25em;
-                z-index: 0;
-              }
-              .item-content {
-                display: flex;
-                flex-direction: row;
-                align-items: center;
-                justify-content: space-between;
-                margin: 0.5em 0;
-              }
-              .svg {
-                z-index: 1;
-              }
-              .active :after {
-                z-index: -1;
-                position: absolute;
-                content: '';
-                height: calc(2em / ${Math.SQRT2} + 3px);
-                width: calc(2em / ${Math.SQRT2} + 3px);
-                right: calc(-1 * calc(calc(2em / ${Math.SQRT2}) / 2) - 3px);
-                top: calc(calc(2em / ${Math.SQRT2}) / 2 - 10px);
-                border-right: 2px solid #000000;
-                border-top: 2px solid #000000;
-                background-color: ${Colors.sbWhite};
-                transform: rotate(45deg);
-              }
-              :not(.active) {
-                background-color: ${Colors.sbWhite};
-              }
-            `}</style>
-          </li>
-        </div>
-      );
-    });
-  }
-
-  renderSubItems(subItems: SubItem[], parentName: string): JSX.Element[] {
-    return subItems.map(({ name, active }: SubItem) => (
-      <li
-        className={`${active && 'active'}`}
-        onClick={e => {
-          this.subItemClicked(e, name);
-        }}
-        key={`${parentName}-${name}`}
-        role="menuitem"
-      >
-        {name}
-        <style jsx>{`
-          li {
-            display: flex;
-            align-items: center;
-            color: ${Colors.sbBlueLighter};
-            font-size: 16px;
-            font-weight: normal;
-            text-indent: 1em;
-            height: 3em;
-          }
-          li :first-child {
-            border-top: 1px solid #000;
-          }
-          p {
-            display: inline-block;
-          }
-          .active {
-            border-left: 3px solid ${Colors.sbBlueLighter};
-            font-weight: bold;
-          }
-          :not(.active) {
-            background-color: ${Colors.sbWhite};
-          }
-        `}</style>
-      </li>
-    ));
-  }
+  };
 
   render() {
     return (
-      <ul>
-        {this.renderItems(this.state.items)}
+      <React.Fragment>
+        <ul role="menu">
+          {this.state.items.map(({ name, subItems, expanded, active }: ItemProps) => {
+            return (
+              <Item
+                name={name}
+                subItems={subItems}
+                expanded={expanded}
+                active={active}
+                activate={this.itemClicked}
+                expand={this.expand}
+                key={name}
+              >
+                {subItems && (
+                  <ul className={`sub-list ${expanded ? 'expanded' : ''}`}>
+                    {subItems.map(subItem => {
+                      return (
+                        <SubItem
+                          name={subItem.name}
+                          active={subItem.active}
+                          activate={this.subItemClicked}
+                          key={`${name}-${subItem.name}`}
+                        />
+                      );
+                    })}
+                  </ul>
+                )}
+              </Item>
+            );
+          })}
+        </ul>
         <style jsx>{`
+          * {
+            font-family: 'PT Sans Caption';
+          }
           ul {
             list-style-type: none;
             padding-left: 0;
             border-width: 0 1px 1px 0;
             border-style: solid;
-            width: 15em;
+            width: auto;
+          }
+          .sub-list {
+            border: none;
+            width: 100%;
+          }
+          .sub-list .expanded {
+            height: auto;
+          }
+          .sub-list :not(.expanded) {
+            display: none;
+            height: 0;
+            overflow: hidden;
           }
         `}</style>
-      </ul>
+      </React.Fragment>
     );
   }
 }
