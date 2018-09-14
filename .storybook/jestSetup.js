80c8b590f2a9e2b383052b529b34b9d8e3eb4fc3
Henry Peterson <peterhen@oregonstate.edu>
2018-09-14 12:48:40 -0700

    chore(jest): setup enzyme to test ui components

--- .storybook/jestSetup.js
+++ .storybook/jestSetup.js
@@ -1,2 +1,6 @@
 import registerRequireContextHook from 'babel-plugin-require-context-hook/register';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 registerRequireContextHook();
