d9506e5e2695d3b967d2ae3bf92297f8d841248e
Henry Peterson <peterhen@oregonstate.edu>
2018-09-19 14:43:21 -0700

    feat(additional-materials): add AdditionalMaterials component

---
+++ src/components/AdditionalMaterials/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { HelpCircle } from 'react-feather';
+import { Styles } from '../../constants';
+import { SbNavLink } from '../SbNavLink';
+
+export const AdditionalMaterials = (): JSX.Element => {
+  const sampleItemsContent = 'Sample Items, Scoring Guides and Rubrics';
+  const annotatedContent = 'Annotated Student Work';
+  const digitalLibraryContent = 'Digital Library Resources';
+  const buildContent = (content: string): JSX.Element => (
+    <div>
+      <HelpCircle />
+      <p>{content}</p>
+      <style jsx>{`
+        div {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+        }
+        p {
+          font-family: ${Styles.sbSans};
+          padding: 0 10px;
+        }
+      `}</style>
+    </div>
+  );
+
+  return (
+    <div>
+      <SbNavLink url={''} content={buildContent(sampleItemsContent)} />
+      <SbNavLink url={''} content={buildContent(annotatedContent)} />
+      <SbNavLink url={''} content={buildContent(digitalLibraryContent)} />
+    </div>
+  );
+};
