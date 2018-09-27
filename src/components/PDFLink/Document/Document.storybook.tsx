5e2adf8408a77065911cfc2727b5d22cfdcb6d6f
Graham Barber <grahammbarber@gmail.com>
2018-09-27 11:25:15 -0700

    test(pdflink): add document ELA and Math stories

---
+++ src/components/PDFLink/Document/Document.storybook.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { PDFViewer } from '@react-pdf/renderer';
+
+import { CustomDocument } from './';
+
+const iframeStyle = {
+  width: 'calc(100vw - 20px)',
+  height: 'calc(100vh - 20px)'
+};
+
+storiesOf('PDFLink/Document', module)
+  .add('ELA document', () => <PDFViewer style={iframeStyle}>{CustomDocument}</PDFViewer>)
+  .add('Math document', () => <PDFViewer style={iframeStyle}>{CustomDocument}</PDFViewer>);
