efaa70abe1d677add62469570f96c10cf82e0483
Graham Barber <grahammbarber@gmail.com>
2018-09-27 14:04:43 -0700

    fix(pdflink): workaround Safari animation bug

--- src/components/PDFLink/index.tsx
+++ src/components/PDFLink/index.tsx
@@ -2,56 +2,36 @@ import React from 'react';
 import { Download, Loader } from 'react-feather';
 import { PDFDownloadLink } from '@react-pdf/renderer';
 
+import { Loading } from '../Loading';
 import { CustomDocument } from './Document';
 
-const renderIcon = (loading: boolean): JSX.Element => {
-  if (loading) {
-    return (
-      <div className="loader">
-        <Loader />
-        <style jsx>{`
-          .loader {
-            display: flex;
-            align-items: center;
-            animation: rotate 1s linear infinite;
-          }
-
-          @keyframes rotate {
-            100% {
-              transform: rotate(360deg);
-            }
-          }
-        `}</style>
-      </div>
-    );
-  }
-
-  return (
-    <div className="download">
-      <p>Download as PDF</p>
-      <Download />
-      <style jsx>{`
-        .download {
-          display: flex;
-          align-items: center;
-        }
-
-        p {
-          font-size: 1em;
-        }
-      `}</style>
-    </div>
-  );
-};
-
-interface PDFLinkRenderProps {
+interface PDFDownloadLinkRenderProps {
   loading: boolean;
 }
+
+export const DownloadIcon = () => (
+  <div className="download">
+    <Download />
+    <p>Download PDF</p>
+    <style jsx>{`
+      .download {
+        font-family: 'PT Sans Caption', sans-serif;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+
+      p {
+        margin-top: 0.5em;
+      }
+    `}</style>
+  </div>
+);
 
 export const PDFLink = () => (
   <div>
     <PDFDownloadLink document={CustomDocument} fileName="somename.pdf">
-      {({ loading }: PDFLinkRenderProps) => renderIcon(loading)}
+      {({ loading }: PDFDownloadLinkRenderProps) => (loading ? <Loading /> : <DownloadIcon />)}
     </PDFDownloadLink>
   </div>
 );
