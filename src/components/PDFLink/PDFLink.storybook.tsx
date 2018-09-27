3adda02996cef1249c7ca232b717a1d590de05c7
Graham Barber <grahammbarber@gmail.com>
2018-09-27 14:05:45 -0700

    test(pdflink): add download indicator story

--- src/components/PDFLink/PDFLink.storybook.tsx
+++ src/components/PDFLink/PDFLink.storybook.tsx
@@ -3,9 +3,10 @@ import { storiesOf } from '@storybook/react';
 import centered from '@storybook/addon-centered';
 import { PDFViewer } from '@react-pdf/renderer';
 
-import { PDFLink } from './';
+import { PDFLink, DownloadIcon } from './';
 import { CustomDocument } from './Document';
 
 storiesOf('PDFLink', module)
   .addDecorator(centered)
-  .add('Default', () => <PDFLink />);
+  .add('Download Link', () => <PDFLink />)
+  .add('Download Indicator', () => <DownloadIcon />);
