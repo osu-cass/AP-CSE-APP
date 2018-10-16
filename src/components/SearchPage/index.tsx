5172876f81b183a445820e958341495c5b4cd7c5
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2018-10-16 13:01:23 -0700

    feat(search-page): make search page component stateful

--- src/components/SearchPage/index.tsx
+++ src/components/SearchPage/index.tsx
@@ -3,11 +3,20 @@ import { Search } from 'react-feather';
 import { Colors, blueGradientBgImg } from '../../constants';
 import { SearchBar } from '../SearchBar';
 import { TitleBar } from '../TitleBar';
-import { FilterItemList, FilterItemListProps } from '../FilterItemList';
+import { FilterItemList } from '../FilterItemList';
+import { Filter } from '../Filter';
+import { CSEFilterOptions, CSEFilterParams } from '../../models/filter';
+import { FilterItemProps } from '../FilterItem';
 
 export interface SearchPageProps {
   pageTitle: string;
-  results: FilterItemListProps;
+  filterOptions: CSEFilterOptions;
+  searchApi: (search: string) => FilterItemProps[];
+}
+
+export interface SearchPageState {
+  filterParams: CSEFilterParams;
+  results: FilterItemProps[];
 }
 
 const style = {
@@ -15,13 +24,42 @@ const style = {
   maxWidth: '1024px'
 };
 
-export const SearchPage = (props: SearchPageProps): JSX.Element => (
-  <div>
-    <div style={style}>
-      <TitleBar claimTitle={props.pageTitle} />
-    </div>
-    <SearchBar />
-    <div>This is a placeholder for the filter component</div>
-    <FilterItemList {...props.results} />
-  </div>
-);
+/**
+ * Search page react component. Handles text searching and filtering
+ *
+ * @export
+ * @class SearchPage
+ * @extends {React.Component<SearchPageProps, SearchPageState>}
+ */
+export class SearchPage extends React.Component<SearchPageProps, SearchPageState> {
+  constructor(props: SearchPageProps) {
+    super(props);
+
+    this.state = {
+      filterParams: { grades: [] },
+      results: []
+    };
+  }
+
+  updateFilter = (newFilter: CSEFilterParams) => {
+    this.setState({ filterParams: newFilter });
+  };
+
+  render() {
+    const { pageTitle, filterOptions } = this.props;
+    const { filterParams, results } = this.state;
+
+    return (
+      <div>
+        <div style={style}>
+          <TitleBar claimTitle={pageTitle} />
+        </div>
+        <SearchBar />
+        <div>
+          <Filter options={filterOptions} params={filterParams} onUpdate={this.updateFilter} />
+        </div>
+        <FilterItemList allItems={results} />
+      </div>
+    );
+  }
+}
