4b050b306f3cd3f9440506f6e0307b85cf7af90e
Dan Van Horn <dan.vanhorn17@gmail.com>
2018-10-22 16:01:53 -0700

    feat(search): adds method to build url

--- src/clients/search/index.ts
+++ src/clients/search/index.ts
@@ -1,7 +1,11 @@
 import { IClaim } from '../../models/claim';
+import { ITargetParams } from '../target';
 
+export interface ISearchParams extends ITargetParams {
+  query?: string;
+}
 export interface ISearchClient {
-  search: () => Promise<IClaim>;
+  search: (params: ISearchParams) => Promise<IClaim | Error>;
 }
 
 /**
@@ -15,12 +19,40 @@ export class SearchClient implements ISearchClient {
     this.endpoint = 'https://localhost:3000';
   }
 
-  public async search(): Promise<IClaim> {
-    return new Promise<IClaim>((resolve, reject) => {
-      const claim: Partial<IClaim> = {
-        title: 'test claim'
-      };
-      resolve(<IClaim>claim);
-    });
+  private buildParams(params: ISearchParams): string {
+    const { query, subject, grades, claim, targetShortCode } = params;
+    let url = `${this.endpoint}/api/search/?`;
+
+    if (query) {
+      url = url.concat(url, `query=${query}&`);
+    }
+    if (subject) {
+      url = url.concat(url, `subject=${subject}`);
+    }
+    if (grades) {
+      url = url.concat(url, `&grades=${grades}`);
+    }
+    if (claim) {
+      url = url.concat(url, `&claimNumber=${claim}`);
+    }
+    if (targetShortCode) {
+      url = url.concat(url, `&targetShortCode=${targetShortCode}`);
+    }
+
+    return url;
+  }
+
+  public async search(params: ISearchParams): Promise<IClaim | Error> {
+    const url: string = this.buildParams(params);
+    let claim: IClaim;
+
+    try {
+      const response: Response = await window.fetch(url);
+      claim = <IClaim>await response.json();
+    } catch (err) {
+      throw new Error('Search failed.');
+    }
+
+    return claim;
   }
 }
