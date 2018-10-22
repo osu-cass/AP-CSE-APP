dbd2c8417dfad4b9d8ce12af0ebaab0a280d27d9
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2018-10-22 15:36:20 -0700

    test(filter): refactor fitler tests to be somewhat less repetitive

--- src/components/Filter/FilterHelper.spec.tsx
+++ src/components/Filter/FilterHelper.spec.tsx
@@ -6,6 +6,8 @@ import {
   FilterOptionModel
 } from '@osu-cass/sb-components';
 
+// tslint:disable:no-non-null-assertion
+
 describe('FilterHelper.createFilters for grade', () => {
   let expectation: AdvancedFilterCategoryModel;
   beforeEach(() => {
@@ -24,58 +26,36 @@ describe('FilterHelper.createFilters for grade', () => {
     };
   });
 
-  it('grade no params', () => {
+  it('no params', () => {
     const params = { grades: [] };
     const result = createFilters(filterOptions, params);
 
     expect(result.gradeFilter).toEqual(expectation);
   });
 
-  it('grade single selected', () => {
+  it('single selected', () => {
     const params = { grades: ['ms'] };
     const result = createFilters(filterOptions, params);
 
-    const expectation: AdvancedFilterCategoryModel = {
-      isMultiSelect: true,
-      displayAllButton: false,
-      disabled: false,
-      label: 'Grade',
-      code: FilterType.Grade,
-      filterOptions: filterOptions.grades.map(g => ({
-        key: g.code,
-        label: g.label,
-        isSelected: g.code === 'ms',
-        filterType: FilterType.Grade
-      }))
-    };
+    expectation.filterOptions.find(f => f.key === 'ms')!.isSelected = true;
     expect(result.gradeFilter).toEqual(expectation);
   });
 
-  it('grade multiple selected', () => {
+  it('multiple selected', () => {
     const params = { grades: ['ms', 'hs'] };
     const result = createFilters(filterOptions, params);
 
-    const expectation: AdvancedFilterCategoryModel = {
-      isMultiSelect: true,
-      displayAllButton: false,
-      disabled: false,
-      label: 'Grade',
-      code: FilterType.Grade,
-      filterOptions: filterOptions.grades.map(g => ({
-        key: g.code,
-        label: g.label,
-        isSelected: g.code === 'ms' || g.code === 'hs',
-        filterType: FilterType.Grade
-      }))
-    };
+    expectation.filterOptions.find(f => f.key === 'ms')!.isSelected = true;
+    expectation.filterOptions.find(f => f.key === 'hs')!.isSelected = true;
+
     expect(result.gradeFilter).toEqual(expectation);
   });
+});
 
-  it('subject none selected', () => {
-    const params = { grades: [] };
-    const result = createFilters(filterOptions, params);
-
-    const expectation: AdvancedFilterCategoryModel = {
+describe('FilterHelper.createFilters for subject', () => {
+  let expectation: AdvancedFilterCategoryModel;
+  beforeEach(() => {
+    expectation = {
       isMultiSelect: false,
       displayAllButton: true,
       disabled: false,
@@ -88,34 +68,28 @@ describe('FilterHelper.createFilters for grade', () => {
         filterType: FilterType.Subject
       }))
     };
+  });
+
+  it('none selected', () => {
+    const params = { grades: [] };
+    const result = createFilters(filterOptions, params);
+
     expect(result.subjectFilter).toEqual(expectation);
   });
 
-  it('subject math selected', () => {
+  it('math selected', () => {
     const params = { grades: [], subject: 'math' };
     const result = createFilters(filterOptions, params);
 
-    const expectation: AdvancedFilterCategoryModel = {
-      isMultiSelect: false,
-      displayAllButton: true,
-      disabled: false,
-      label: 'Subject',
-      code: FilterType.Subject,
-      filterOptions: filterOptions.subjects.map(s => ({
-        key: s.code,
-        label: s.label,
-        isSelected: s.code === 'math',
-        filterType: FilterType.Subject
-      }))
-    };
+    expectation.filterOptions.find(f => f.key === 'math')!.isSelected = true;
     expect(result.subjectFilter).toEqual(expectation);
   });
+});
 
-  it('claim none selected', () => {
-    const params = { grades: [], subject: 'math' };
-    const result = createFilters(filterOptions, params);
-
-    const expectation: AdvancedFilterCategoryModel = {
+describe('FilterHelper.createFilters for claim', () => {
+  let expectation: AdvancedFilterCategoryModel;
+  beforeEach(() => {
+    expectation = {
       isMultiSelect: false,
       displayAllButton: true,
       disabled: false,
@@ -136,42 +110,28 @@ describe('FilterHelper.createFilters for grade', () => {
         }
       ]
     };
+  });
+
+  it('none selected', () => {
+    const params = { grades: [], subject: 'math' };
+    const result = createFilters(filterOptions, params);
+
     expect(result.claimFilter).toEqual(expectation);
   });
 
-  it('claim m1 selected', () => {
+  it('m1 selected', () => {
     const params = { grades: [], subject: 'math', claim: 'm1' };
     const result = createFilters(filterOptions, params);
 
-    const expectation: AdvancedFilterCategoryModel = {
-      isMultiSelect: false,
-      displayAllButton: true,
-      disabled: false,
-      label: 'Claim',
-      code: FilterType.Claim,
-      filterOptions: [
-        {
-          key: 'm1',
-          label: 'Problem Solving',
-          isSelected: true,
-          filterType: FilterType.Claim
-        },
-        {
-          key: 'm2',
-          label: 'Other math stuff',
-          isSelected: false,
-          filterType: FilterType.Claim
-        }
-      ]
-    };
+    expectation.filterOptions.find(f => f.key === 'm1')!.isSelected = true;
     expect(result.claimFilter).toEqual(expectation);
   });
+});
 
-  it('target none selected', () => {
-    const params = { grades: [], subject: 'math', claim: 'm1' };
-    const result = createFilters(filterOptions, params);
-
-    const expectation: AdvancedFilterCategoryModel = {
+describe('FilterHelper.createFilters for target', () => {
+  let expectation: AdvancedFilterCategoryModel;
+  beforeEach(() => {
+    expectation = {
       isMultiSelect: false,
       displayAllButton: true,
       disabled: false,
@@ -192,6 +152,12 @@ describe('FilterHelper.createFilters for grade', () => {
         }
       ]
     };
+  });
+
+  it('none selected', () => {
+    const params = { grades: [], subject: 'math', claim: 'm1' };
+    const result = createFilters(filterOptions, params);
+
     expect(result.targetFilter).toEqual(expectation);
   });
 
@@ -199,27 +165,7 @@ describe('FilterHelper.createFilters for grade', () => {
     const params = { grades: [], subject: 'math', claim: 'm1', target: 'pf' };
     const result = createFilters(filterOptions, params);
 
-    const expectation: AdvancedFilterCategoryModel = {
-      isMultiSelect: false,
-      displayAllButton: true,
-      disabled: false,
-      label: 'Target',
-      code: FilterType.Target,
-      filterOptions: [
-        {
-          key: 'sr',
-          label: 'Short Response',
-          isSelected: false,
-          filterType: FilterType.Target
-        },
-        {
-          key: 'pf',
-          label: 'Proof',
-          isSelected: true,
-          filterType: FilterType.Target
-        }
-      ]
-    };
+    expectation.filterOptions.find(f => f.key === 'pf')!.isSelected = true;
     expect(result.targetFilter).toEqual(expectation);
   });
 
@@ -302,47 +248,38 @@ describe('FilterHelper.sanitizeParams', () => {
 });
 
 describe('FilterHelper.paramsFromFilter', () => {
+  const defaultParams = { grades: ['ms', 'hs'], subject: 'math', claim: 'm1', target: 'pf' };
   it('no change, type grade', () => {
-    const params = { grades: ['ms', 'hs'], subject: 'math', claim: 'm1', target: 'pf' };
-    const result = paramsFromFilter(params, FilterType.Grade, undefined);
+    const result = paramsFromFilter(defaultParams, FilterType.Grade, undefined);
 
     const expected = { grades: [], subject: 'math', claim: 'm1', target: 'pf' };
     expect(result).toEqual(expected);
   });
 
   it('no change, type subject', () => {
-    const params = { grades: ['ms', 'hs'], subject: 'math', claim: 'm1', target: 'pf' };
-    const result = paramsFromFilter(params, FilterType.Subject, undefined);
+    const result = paramsFromFilter(defaultParams, FilterType.Subject, undefined);
 
     const expected = { grades: ['ms', 'hs'], claim: 'm1', target: 'pf' };
     expect(result).toEqual(expected);
   });
 
   it('no change, type claim', () => {
-    const params = { grades: ['ms', 'hs'], subject: 'math', claim: 'm1', target: 'pf' };
-    const result = paramsFromFilter(params, FilterType.Claim, undefined);
+    const result = paramsFromFilter(defaultParams, FilterType.Claim, undefined);
 
     const expected = { grades: ['ms', 'hs'], subject: 'math', target: 'pf' };
     expect(result).toEqual(expected);
   });
 
   it('no change, type target', () => {
-    const params = { grades: ['ms', 'hs'], subject: 'math', claim: 'm1', target: 'pf' };
-    const result = paramsFromFilter(params, FilterType.Target, undefined);
+    const result = paramsFromFilter(defaultParams, FilterType.Target, undefined);
 
     const expected = { grades: ['ms', 'hs'], subject: 'math', claim: 'm1' };
     expect(result).toEqual(expected);
   });
 
   it('grade added', () => {
-    const params = { grades: ['ms', 'hs'], subject: 'math', claim: 'm1', target: 'pf' };
-    const change = {
-      label: 'Elementary',
-      key: 'elem',
-      isSelected: false,
-      filterType: FilterType.Grade
-    };
-    const result = paramsFromFilter(params, FilterType.Grade, change);
+    const change = { label: '', key: 'elem', isSelected: false, filterType: FilterType.Grade };
+    const result = paramsFromFilter(defaultParams, FilterType.Grade, change);
 
     const expected = { grades: ['ms', 'hs', 'elem'], subject: 'math', claim: 'm1', target: 'pf' };
     expect(result).toEqual(expected);
@@ -350,12 +287,7 @@ describe('FilterHelper.paramsFromFilter', () => {
 
   it('grade removed', () => {
     const params = { grades: ['ms', 'hs', 'elem'], subject: 'math', claim: 'm1', target: 'pf' };
-    const change = {
-      label: 'Elementary',
-      key: 'elem',
-      isSelected: true,
-      filterType: FilterType.Grade
-    };
+    const change = { label: '', key: 'elem', isSelected: true, filterType: FilterType.Grade };
     const result = paramsFromFilter(params, FilterType.Grade, change);
 
     const expected = { grades: ['ms', 'hs'], subject: 'math', claim: 'm1', target: 'pf' };
@@ -364,12 +296,7 @@ describe('FilterHelper.paramsFromFilter', () => {
 
   it('grade added, already there', () => {
     const params = { grades: ['ms', 'hs', 'elem'], subject: 'math', claim: 'm1', target: 'pf' };
-    const change = {
-      label: 'Elementary',
-      key: 'elem',
-      isSelected: false,
-      filterType: FilterType.Grade
-    };
+    const change = { label: '', key: 'elem', isSelected: false, filterType: FilterType.Grade };
     const result = paramsFromFilter(params, FilterType.Grade, change);
 
     const expected = { grades: ['ms', 'hs', 'elem'], subject: 'math', claim: 'm1', target: 'pf' };
@@ -378,12 +305,7 @@ describe('FilterHelper.paramsFromFilter', () => {
 
   it('grade removed, already gone', () => {
     const params = { grades: ['ms', 'hs'], subject: 'math', claim: 'm1', target: 'pf' };
-    const change = {
-      label: 'Elementary',
-      key: 'elem',
-      isSelected: true,
-      filterType: FilterType.Grade
-    };
+    const change = { label: '', key: 'elem', isSelected: true, filterType: FilterType.Grade };
     const result = paramsFromFilter(params, FilterType.Grade, change);
 
     const expected = { grades: ['ms', 'hs'], subject: 'math', claim: 'm1', target: 'pf' };
@@ -392,12 +314,7 @@ describe('FilterHelper.paramsFromFilter', () => {
 
   it('subject selected', () => {
     const params = { grades: ['ms', 'hs'], subject: undefined, claim: 'm1', target: 'pf' };
-    const change = {
-      label: 'Math',
-      key: 'math',
-      isSelected: false,
-      filterType: FilterType.Subject
-    };
+    const change = { label: '', key: 'math', isSelected: false, filterType: FilterType.Subject };
     const result = paramsFromFilter(params, FilterType.Subject, change);
 
     const expected = { grades: ['ms', 'hs'], subject: 'math', claim: 'm1', target: 'pf' };
@@ -407,7 +324,7 @@ describe('FilterHelper.paramsFromFilter', () => {
   it('subject deselected', () => {
     const params = { grades: ['ms', 'hs'], subject: 'math', claim: 'm1', target: 'pf' };
     const change = {
-      label: 'Math',
+      label: '',
       key: 'math',
       isSelected: true,
       filterType: FilterType.Subject
@@ -420,12 +337,7 @@ describe('FilterHelper.paramsFromFilter', () => {
 
   it('claim selected', () => {
     const params = { grades: ['ms', 'hs'], subject: 'math' };
-    const change = {
-      label: 'Problem Solving',
-      key: 'm1',
-      isSelected: false,
-      filterType: FilterType.Claim
-    };
+    const change = { label: '', key: 'm1', isSelected: false, filterType: FilterType.Claim };
     const result = paramsFromFilter(params, FilterType.Claim, change);
 
     const expected = { grades: ['ms', 'hs'], subject: 'math', claim: 'm1' };
@@ -434,12 +346,7 @@ describe('FilterHelper.paramsFromFilter', () => {
 
   it('claim deselected', () => {
     const params = { grades: ['ms', 'hs'], subject: 'math', claim: 'm1' };
-    const change = {
-      label: 'Problem Solving',
-      key: 'm1',
-      isSelected: true,
-      filterType: FilterType.Claim
-    };
+    const change = { label: '', key: 'm1', isSelected: true, filterType: FilterType.Claim };
     const result = paramsFromFilter(params, FilterType.Claim, change);
 
     const expected = { grades: ['ms', 'hs'], subject: 'math' };
@@ -448,12 +355,7 @@ describe('FilterHelper.paramsFromFilter', () => {
 
   it('target selected', () => {
     const params = { grades: ['ms', 'hs'], subject: 'math', claim: 'm1' };
-    const change = {
-      label: 'Proof',
-      key: 'pf',
-      isSelected: false,
-      filterType: FilterType.Target
-    };
+    const change = { label: '', key: 'pf', isSelected: false, filterType: FilterType.Target };
     const result = paramsFromFilter(params, FilterType.Target, change);
 
     const expected = { grades: ['ms', 'hs'], subject: 'math', claim: 'm1', target: 'pf' };
@@ -462,15 +364,12 @@ describe('FilterHelper.paramsFromFilter', () => {
 
   it('target deselected', () => {
     const params = { grades: ['ms', 'hs'], subject: 'math', claim: 'm1', target: 'pf' };
-    const change = {
-      label: 'Proof',
-      key: 'pf',
-      isSelected: true,
-      filterType: FilterType.Target
-    };
+    const change = { label: '', key: 'pf', isSelected: true, filterType: FilterType.Target };
     const result = paramsFromFilter(params, FilterType.Target, change);
 
     const expected = { grades: ['ms', 'hs'], subject: 'math', claim: 'm1' };
     expect(result).toEqual(expected);
   });
 });
+
+// tslint:enable:no-non-null-assertion
