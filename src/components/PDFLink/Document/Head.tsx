1661bba83e07869b798dd7e4a59b59bdf156f855
baileysingleton <baileys@osuosl.org>
2018-10-31 10:08:08 -0700

    feat(pdfgen): add header component

---
+++ src/components/PDFLink/Document/Head.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+// import '../../../../node_modules/typeface-pt-serif/index.css';
+import ReactPDF, { Text, View, StyleSheet, Image } from '@react-pdf/renderer';
+import { Colors, Styles } from '../../../constants';
+
+interface HeadStyles {
+  header: object;
+  headerText: object;
+  logo: object;
+  image: object;
+}
+
+const styles: HeadStyles = StyleSheet.create({
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+    fontSize: 12
+  },
+  headerText: {
+    minWidth: '80%',
+    marginBottom: '10px'
+  },
+  logo: {
+    width: '20%',
+    marginBottom: '10px'
+  },
+  image: {}
+}) as HeadStyles;
+
+/*tslint:disable: no-require-im <Image fixed style={styles.logo} src={sbLogo}/>;ports no-var-requires */
+// tslint:disable-next-line:no-require-imports
+const sbLogo: string = require('@sbac/sbac-ui-kit/src/images/SmarterBalanced-Logo.png') as string;
+
+export interface HeadProps {
+  text: string;
+}
+
+export const Head = ({ text }: HeadProps) => (
+  <View fixed style={styles.header}>
+    <View style={styles.headerText}>
+      <Text>{text}</Text>
+    </View>
+    <View style={styles.logo}>
+      <Image style={styles.image} src={sbLogo} />
+    </View>
+  </View>
+);
