6d1466612abb1211afc2e84006bdbe5b04b3358e
Horne <hornee@oregonstate.edu>
2018-11-05 13:08:51 -0800

    feat(download-modal): adds selection confirmation page and logic

--- src/components/DownloadModal/index.tsx
+++ src/components/DownloadModal/index.tsx
@@ -20,10 +20,13 @@ export interface DownloadModalProps {
  */
 export interface DownloadModalState {
   showModal: boolean;
+  showHide: string;
+  showMultiSelect: string;
   taskModelButtons: JSX.Element[];
+  selectedList: JSX.Element[];
 }
 
-const buttonStyles = `button {
+const styling = `button {
   display: flex;
   flex-direction: column;
   width: 225px;
@@ -91,10 +94,30 @@ export class DownloadModal extends Component<DownloadModalProps, DownloadModalSt
     super(props);
     this.state = {
       showModal: false,
-      taskModelButtons: this.generateButtons(props)
+      showHide: 'hidden',
+      showMultiSelect: '',
+      taskModelButtons: this.generateButtons(props),
+      selectedList: []
     };
     this.openModal = this.openModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
+    this.handleContinue = this.handleContinue.bind(this);
+    this.handleBackButton = this.handleBackButton.bind(this);
+    this.handleConfirm = this.handleConfirm.bind(this);
+  }
+  /**
+   * Handles the user clicking the Confirm Button
+   *
+   * To Do: Link result to PDF generation/download
+   */
+
+  handleConfirm() {
+    const selectedArr: string[] = [];
+    this.state.taskModelButtons.forEach(task => {
+      if (task.props.toggled === true) {
+        selectedArr.push(task.props.taskName);
+      }
+    });
   }
 
   openModal() {
@@ -105,41 +128,147 @@ export class DownloadModal extends Component<DownloadModalProps, DownloadModalSt
     this.setState({ showModal: false });
   }
 
+  /**
+   * Handles the user clicking the Continue Button
+   *
+   * Toggles hidden selections page and makes an unordered list of selected buttons
+   */
+
   handleContinue() {
-    return;
+    const taskButtons = _.cloneDeep(this.state.taskModelButtons);
+    const selectList: JSX.Element[] = [];
+    this.createSelectionList(taskButtons, selectList);
+    this.setState({
+      showMultiSelect: 'hidden',
+      showHide: '',
+      selectedList: selectList
+    });
   }
 
+  /**
+   * Generates a list of <li> elements containing all selected taskModels
+   *
+   */
+  createSelectionList(taskButtons: JSX.Element[], selectList: JSX.Element[]) {
+    taskButtons.forEach(task => {
+      if (task.props.toggled === true) {
+        selectList.push(
+          <li key={task.props.taskName}>
+            <style jsx>{`
+              li {
+                margin-bottom: 10px;
+              }
+            `}</style>
+            {task.props.taskName}
+          </li>
+        );
+      }
+    });
+  }
+
+  /**
+   * Handles the user clicking the Back Button on the confirm selection screen
+   *
+   * Toggles hidden selections page and clears the list of selected buttons
+   */
+  handleBackButton() {
+    this.setState({
+      showMultiSelect: '',
+      showHide: 'hidden',
+      selectedList: []
+    });
+  }
+
+  /**
+   * Toggles a selected button and handles multi-select/all-select logic
+   *
+   * This function is passed to the child Taskbutton objects as a prop to call onClick
+   */
   toggleButton = (taskName: string) => {
     const taskButtons = _.cloneDeep(this.state.taskModelButtons);
     if (taskName === 'Entire Target') {
-      taskButtons.forEach(task => {
-        task.props.toggle = false;
-        task.props.cName = 'unchecked';
-      });
-      taskButtons[0].props.toggle = true;
-      taskButtons[0].props.cName = 'checked';
-      this.setState({ taskModelButtons: taskButtons });
+      this.toggleEntireTarget(taskButtons);
     } else {
-      const toggledButton = taskButtons.find(task => {
-        return task.props.taskName === taskName;
-      });
-      const idx = taskButtons.findIndex(task => {
-        return task.props.taskName === taskName;
-      });
+      const { toggledButton, idx } = this.getToggled(taskButtons, taskName);
       if (toggledButton !== undefined) {
-        taskButtons[0].props.toggled = false;
-        taskButtons[0].props.cName = 'unchecked';
-        toggledButton.props.toggled = !toggledButton.props.toggled;
-        toggledButton.props.cName =
-          toggledButton.props.cName === 'unchecked' ? 'checked' : 'unchecked';
-        taskButtons[idx] = toggledButton;
-        this.setState({ taskModelButtons: taskButtons });
+        this.toggleSelection(taskButtons, toggledButton, idx);
       }
     }
   };
+  /**
+   * Sets the toggled boolean and checked/unchecked classname for Taskbutton objects
+   *
+   * This function will untoggle the Entire Target button when setting a single Taskbutton
+   */
+  toggleSelection(taskButtons: JSX.Element[], toggledButton: JSX.Element, idx: number) {
+    taskButtons[0].props.toggled = false;
+    taskButtons[0].props.cName = 'unchecked';
+    toggledButton.props.toggled = !toggledButton.props.toggled;
+    toggledButton.props.cName = toggledButton.props.cName === 'unchecked' ? 'checked' : 'unchecked';
+    taskButtons[idx] = toggledButton;
+    this.setState({ taskModelButtons: taskButtons, selectedList: [] });
+  }
+  /**
+   * Returns the object and index for whichever button was clicked/toggled
+   *
+   */
+  getToggled(taskButtons: JSX.Element[], taskName: string) {
+    const toggledButton = taskButtons.find(task => {
+      return task.props.taskName === taskName;
+    });
+    const idx = taskButtons.findIndex(task => {
+      return task.props.taskName === taskName;
+    });
 
+    return { toggledButton, idx };
+  }
+  /**
+   * Toggles the EntireTarget taskbutton while untoggling all others
+   *
+   */
+  toggleEntireTarget(taskButtons: JSX.Element[]) {
+    taskButtons.forEach(task => {
+      task.props.toggle = false;
+      task.props.cName = 'unchecked';
+    });
+    taskButtons[0].props.toggle = true;
+    taskButtons[0].props.cName = 'checked';
+    this.setState({ taskModelButtons: taskButtons, selectedList: [] });
+  }
+  /**
+   * Creates an array of JSX Elements containing TaskButtons to pass to the state
+   *
+   */
   generateButtons(props: DownloadModalProps): JSX.Element[] {
     const taskArray: JSX.Element[] = [];
+    this.generateDefaultButtons(taskArray);
+    for (const task of props.taskModels) {
+      this.generateTaskButtons(taskArray, task);
+    }
+
+    return taskArray;
+  }
+  /**
+   * Takes all Task Models passed in from props and creates corresponding TaskButtons, adding them into an array of JSX Elements
+   *
+   */
+  generateTaskButtons(taskArray: JSX.Element[], task: string) {
+    taskArray.push(
+      <TaskButton
+        toggled={false}
+        cName="unchecked"
+        id={task.replace(' ', '-').toLowerCase()}
+        key={task}
+        taskName={task}
+        toggleParent={this.toggleButton}
+      />
+    );
+  }
+  /**
+   * Creates the 2 default "Entire Target" and "Overview" TaskButtons;
+   *
+   */
+  generateDefaultButtons(taskArray: JSX.Element[]) {
     taskArray.push(
       <TaskButton
         toggled={true}
@@ -160,20 +289,6 @@ export class DownloadModal extends Component<DownloadModalProps, DownloadModalSt
         toggleParent={this.toggleButton}
       />
     );
-    for (const task of props.taskModels) {
-      taskArray.push(
-        <TaskButton
-          toggled={false}
-          cName="unchecked"
-          id={task.replace(' ', '-').toLowerCase()}
-          key={task}
-          taskName={task}
-          toggleParent={this.toggleButton}
-        />
-      );
-    }
-
-    return taskArray;
   }
 
   render() {
@@ -185,10 +300,26 @@ export class DownloadModal extends Component<DownloadModalProps, DownloadModalSt
         <Modal
           isOpen={this.state.showModal}
           onRequestClose={this.closeModal}
-          contentLabel="Download Modal"
+          contentLabel="Download PDF"
           style={customStyles}
         >
-          <form>
+          <form className={this.state.showMultiSelect}>
+            <style jsx>{`
+              .hidden {
+                display: none;
+              }
+              #title-container {
+                text-align: center;
+                font-family: ${Styles.sbSans};
+                font-size: 18px;
+                font-weight: bold;
+                color: ${Colors.sbGray};
+                letter-spacing: ${Styles.sbLetterSpacing};
+                margin-bottom: 5px;
+                margin-top: -10px;
+              }
+            `}</style>
+            <div id="title-container">Download PDF</div>
             <div id="entire-target-btn-container">{this.state.taskModelButtons[0]}</div>
             <div id="scrollable-btn-container">
               <div id="task-models-container">{taskButtons}</div>
@@ -198,8 +329,59 @@ export class DownloadModal extends Component<DownloadModalProps, DownloadModalSt
                 Continue
               </button>
             </div>
-            <style jsx>{buttonStyles}</style>
+            <style jsx>{styling}</style>
           </form>
+          <div id="confirm-selections" className={this.state.showHide}>
+            <style jsx>{`
+              .hidden {
+                display: none;
+              }
+              div {
+                font-family: ${Styles.sbSans};
+                text-align: center;
+                letter-spacing: ${Styles.sbLetterSpacing};
+                color: ${Colors.sbGray};
+              }
+              #selections-title {
+                font-size: 20px;
+                font-weight: bold;
+                margin-right: 35px;
+                margin-left: 35px;
+                margin-bottom: 5px;
+                margin-top: -10px;
+              }
+              #selections-list {
+                overflow-y: auto;
+                height: 200px;
+                margin-left: -20px;
+                text-align: left;
+                font-size: 16px;
+              }
+              #pdf-page-count {
+                font-size: 14px;
+              }
+              #confirm-back-btn-container button {
+                width: 100px;
+                background-image: ${blueGradientBgImg.backgroundImage};
+                margin-top: 10px;
+                display: inline-block;
+                color: ${Colors.sbGrayLighter};
+              }
+            `}</style>
+            <div id="selections-title">Selected Sections</div>
+            <div id="selections-list">
+              <ul>{this.state.selectedList}</ul>
+            </div>
+            <div id="pdf-page-count">This PDF will be X pages</div>
+            <div id="confirm-back-btn-container">
+              <button type="button" id="back-btn" onClick={this.handleBackButton}>
+                Back
+              </button>
+              <button type="button" id="confirm=btn" onClick={this.handleConfirm}>
+                Confirm
+              </button>
+            </div>
+          </div>
         </Modal>
       </div>
     );
