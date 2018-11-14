023f9283636b92fedb2d8b1663a574d0bdf22d2e
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2018-11-14 14:12:21 -0800

    test(generic-component-page): add better mocks

--- src/components/GenericContentPage/index.storybook.tsx
+++ src/components/GenericContentPage/index.storybook.tsx
@@ -5,11 +5,15 @@ import { MobileGenericContentPage } from './mobile';
 import { DesktopHelpPage } from './desktop';
 import { contentSectionsMocks } from './__tests__/mocks';
 
-storiesOf('Help Page', module)
+storiesOf('Generic Content Page', module)
   .add('default', () => (
     <div id="main-content-scroll" style={{ height: '100vh', overflowY: 'scroll' }}>
       <GenericContentPage contentSections={contentSectionsMocks} title="Page title" />
     </div>
   ))
   .add('mobile', () => <MobileGenericContentPage contentSections={contentSectionsMocks} />)
-  .add('desktop', () => <DesktopHelpPage contentSections={contentSectionsMocks} />);
+  .add('desktop', () => (
+    <div id="main-content-scroll" style={{ height: '100vh', overflowY: 'scroll' }}>
+      <DesktopHelpPage contentSections={contentSectionsMocks} />
+    </div>
+  ));
