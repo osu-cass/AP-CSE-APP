322f7055e5095fe56881a3cb3ba1fd9ba0271099
Graham Barber <grahammbarber@gmail.com>
2018-11-13 17:01:10 -0800

    test(development): update story names

--- src/pages/Development/Development.storybook.tsx
+++ src/pages/Development/Development.storybook.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import centered from '@storybook/addon-centered';
 import { DevelopmentPage } from '.';
 import { RouterDecorator } from '../../__decorators__';
 
-storiesOf('TestPage component', module)
+storiesOf('Development Page', module)
   .addDecorator(RouterDecorator)
-  .addDecorator(centered)
-  .add('TestPage', () => <DevelopmentPage />);
+  .add('Default', () => <DevelopmentPage />);
