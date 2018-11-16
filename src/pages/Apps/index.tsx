283ebee2e631a8f115f54f7ee783a8a6c51f2ac6
Benson <bensonre@oregonstate.edu>
2018-11-16 11:11:24 -0800

    fix(apps): add images and allign applinks

--- src/pages/Apps/index.tsx
+++ src/pages/Apps/index.tsx
@@ -4,17 +4,19 @@ import { GenericPage } from '../../components/GenericPage';
 import { AppLink, AppLinkProps } from '../../components/AppLink';
 
 /*tslint:disable: no-require-imports no-var-requires */
-const image: string = require('../../assets/images/smarter-balanced.png') as string;
+const scoreimg: string = require('../../assets/images/score-guide.png') as string;
+const Practiceimg: string = require('../../assets/images/practice-tests.png') as string;
 const sampleItemsImg: string = require('../../assets/images/sample-items.png') as string;
 const digitalLibraryImg: string = require('../../assets/images/digital-library.png') as string;
 
 const scoreGuidesLinkProps: AppLinkProps = {
   title: 'Score Guides',
-  imgUrl: image,
-  desc: 'Score Guides explanation...',
+  imgUrl: scoreimg,
+  desc: 'Practice Test Scoring Guides for grades 3-12 in Math and English Language Arts.',
   linkBtnProps: {
     text: 'Visit Score Guides Page',
-    url: ''
+    url:
+      'http://www.smarterbalanced.org/assessments/practice-and-training-tests/resources-and-documentation/#tab-3'
   }
 };
 const digitalLibraryLinkProps: AppLinkProps = {
@@ -29,11 +31,12 @@ const digitalLibraryLinkProps: AppLinkProps = {
 };
 const practiceTestsLinkProps: AppLinkProps = {
   title: 'Practice Tests',
-  imgUrl: image,
-  desc: 'Practice Tests explanation...',
+  imgUrl: Practiceimg,
+  desc:
+    'Use the same testing software as students to see what they encounter during testing! Try out an English language arts or math test to learn how the test works, what’s expected of students and what kind of questions are included on them.',
   linkBtnProps: {
     text: 'Visit Practice Tests Page',
-    url: ''
+    url: 'http://www.smarterbalanced.org/assessments/samples/'
   }
 };
 const sampleItemsLinkProps: AppLinkProps = {
