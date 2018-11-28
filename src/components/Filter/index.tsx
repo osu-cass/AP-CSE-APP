72efe951949209dd9a70013d67ddf38fa9538b22
Dan Van Horn <dan.vanhorn17@gmail.com>
2018-11-28 15:43:33 -0800

    fix(filter): render dynamic message to promt user to select filter

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -9,6 +9,7 @@ import { createFilters, sanitizeParams, paramsFromFilter } from './FilterHelper'
 import { CSEFilterOptions, CSEFilterParams } from '../../models/filter';
 import { Colors, blueGradient, Styles } from '../../constants';
 import css from 'styled-jsx/css';
+import { Message } from './Messages';
 
 const globalFilterStyle = css`
   .filter-selection {
@@ -62,10 +63,13 @@ export interface CSEAdvancedFilterModels {
 
 export const Filter: React.SFC<FilterProps> = ({ options, params, onUpdate }) => {
   const cleanParams = sanitizeParams(params, options);
-  const { gradeFilter, subjectFilter, claimFilter, targetFilter } = createFilters(
-    options,
-    cleanParams
-  );
+
+  const {
+    gradeFilter,
+    subjectFilter,
+    claimFilter,
+    targetFilter
+  }: CSEAdvancedFilterModels = createFilters(options, cleanParams);
 
   const callback = (filterType: FilterType, data?: FilterOptionModel) => {
     const newParams = paramsFromFilter(cleanParams, filterType, data);
@@ -76,23 +80,64 @@ export const Filter: React.SFC<FilterProps> = ({ options, params, onUpdate }) =>
     onUpdate({ grades: [], subject: undefined, claim: undefined, target: undefined });
   };
 
-  const filterJsx = [gradeFilter, subjectFilter, claimFilter, targetFilter].map((f, i) =>
-    f ? (
-      <AdvancedFilter
-        key={i}
-        {...f}
-        onFilterOptionSelect={data => {
-          callback(f.code, data);
-        }}
-      />
-    ) : (
-      undefined
-    )
-  );
+  const renderClaimFilter = (): JSX.Element | undefined => {
+    let content: JSX.Element | undefined;
+    if (claimFilter) {
+      content = (
+        <AdvancedFilter
+          key={claimFilter.label}
+          {...claimFilter}
+          onFilterOptionSelect={(data: FilterOptionModel) => {
+            callback(claimFilter.code, data);
+          }}
+        />
+      );
+    } else {
+      content = <Message>Please select a Grade and Subject</Message>;
+    }
+
+    return content;
+  };
+
+  const renderTargetFilter = (): JSX.Element | undefined => {
+    let content: JSX.Element | undefined;
+    if (claimFilter && targetFilter) {
+      content = (
+        <AdvancedFilter
+          key={targetFilter.label}
+          {...targetFilter}
+          onFilterOptionSelect={(data: FilterOptionModel) => {
+            callback(targetFilter.code, data);
+          }}
+        />
+      );
+    } else if (claimFilter) {
+      content = <Message>Please select a Claim.</Message>;
+    }
+
+    return content;
+  };
 
   return (
     <Fragment>
-      <div className="filter">{filterJsx}</div>
+      <div className="filter">
+        <AdvancedFilter
+          key={gradeFilter.label}
+          {...gradeFilter}
+          onFilterOptionSelect={(data: FilterOptionModel) => {
+            callback(gradeFilter.code, data);
+          }}
+        />
+        <AdvancedFilter
+          key={subjectFilter.label}
+          {...subjectFilter}
+          onFilterOptionSelect={(data: FilterOptionModel) => {
+            callback(subjectFilter.code, data);
+          }}
+        />
+        {renderClaimFilter()}
+        {renderTargetFilter()}
+      </div>
       <div className="reset-container">
         <button className="btn" onClick={reset}>
           Reset
