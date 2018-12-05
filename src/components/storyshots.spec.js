21d6654e615b333cc3bc7c5978edcfde5fabcf8b
Thomas Noelcke <noelcket@oregonstate.edu>
2018-12-05 09:51:34 -0800

    test(storyshot): remove unused test options

--- src/components/storyshots.spec.js
+++ src/components/storyshots.spec.js
@@ -1,10 +1,6 @@
 import initStoryshots, { multiSnapshotWithOptions, Stories } from '@storybook/addon-storyshots';
-import { mount } from 'enzyme';
-import toJSON from 'enzyme-to-json';
 
 initStoryshots({
-  renderer: mount,
-  serializer: toJSON,
   test: multiSnapshotWithOptions({}),
   storyKindRegex: /^((?!.*DontTest).)*$/
 });
