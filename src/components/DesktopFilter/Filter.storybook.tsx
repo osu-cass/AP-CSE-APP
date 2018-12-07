17e77ecf7e47ad8122db41bd662ffdffcc5703d4
Thomas Noelcke <noelcket@oregonstate.edu>
2018-12-07 12:42:07 -0800

    test(desktop filter): update desktop filter storybook

--- src/components/DesktopFilter/Filter.storybook.tsx
+++ src/components/DesktopFilter/Filter.storybook.tsx
@@ -1,62 +1,27 @@
 import React from 'react';
 import { CSEFilterParams, CSEFilterOptions } from '../../models/filter';
 import { DesktopFilter } from '.';
-import { filterOptionsGS, filterOptionsGSC, filterOptionsGSCT } from './__mocks__';
+import { filterModelGS, filterModelGSC, filterModelCSCT } from '../MobileFilter/mocks';
 import { storiesOf } from '@storybook/react';
 import centered from '@storybook/addon-centered';
+import { FilterOptionModel, FilterType } from '@osu-cass/sb-components';
 
-const callback = (newFilter: CSEFilterParams) => {
+const callback = (Code: FilterType, newFilter?: FilterOptionModel) => {
   // tslint:disable-next-line:no-console
   console.log('New filter params:', JSON.stringify(newFilter));
 };
 
+const reset = () => {
+  // tslint:disable-next-line:no-console
+  console.log('Reset Pressed');
+};
+
 storiesOf('Desktop Filter', module)
   .addDecorator(centered)
-  .add('default', () => (
-    <DesktopFilter params={{ grades: [] }} options={filterOptionsGS} onUpdate={callback} />
+  .add('default', () => <DesktopFilter filters={filterModelGS} onUpdate={callback} reset={reset} />)
+  .add('Subject, Grade and Claim', () => (
+    <DesktopFilter filters={filterModelGSC} onUpdate={callback} reset={reset} />
   ))
-  .add('selected grade', () => (
-    <DesktopFilter params={{ grades: ['ms'] }} options={filterOptionsGS} onUpdate={callback} />
-  ))
-  .add('selected subject', () => (
-    <DesktopFilter
-      params={{ grades: [], subject: 'math' }}
-      options={filterOptionsGSC}
-      onUpdate={callback}
-    />
-  ))
-  .add('selected subject, claim', () => (
-    <DesktopFilter
-      params={{ grades: [], subject: 'math', claim: 'm1' }}
-      options={filterOptionsGSCT}
-      onUpdate={callback}
-    />
-  ))
-  .add('selected subject, claim, target', () => (
-    <DesktopFilter
-      params={{ grades: [], subject: 'math', claim: 'm1', target: 'pf' }}
-      options={filterOptionsGSCT}
-      onUpdate={callback}
-    />
-  ))
-  .add('selected grades, subject, claim, target', () => (
-    <DesktopFilter
-      params={{ grades: ['ms', 'hs'], subject: 'math', claim: 'm1', target: 'pf' }}
-      options={filterOptionsGSCT}
-      onUpdate={callback}
-    />
-  ))
-  .add('invalid', () => (
-    <DesktopFilter
-      params={{ grades: ['ms'], claim: 'm1', target: 'pf' }}
-      options={filterOptionsGS}
-      onUpdate={callback}
-    />
-  ))
-  .add('invalid 2', () => (
-    <DesktopFilter
-      params={{ grades: ['ms'], subject: 'math', target: 'pf' }}
-      options={filterOptionsGSC}
-      onUpdate={callback}
-    />
+  .add('Subject grade and target', () => (
+    <DesktopFilter filters={filterModelCSCT} onUpdate={callback} reset={reset} />
   ));
