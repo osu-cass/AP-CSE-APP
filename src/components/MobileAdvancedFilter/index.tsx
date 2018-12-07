4a3bf39221bcbc742f17dc1ac1e7377769e0c931
Thomas Noelcke <noelcket@oregonstate.edu>
2018-12-07 09:42:53 -0800

    feat(mobile page): add storybook for mobile page

--- src/components/MobileAdvancedFilter/index.tsx
+++ src/components/MobileAdvancedFilter/index.tsx
@@ -11,7 +11,7 @@ export interface MobileFilterProps extends AdvancedFilterCategoryModel {
   onMobileSelect: (selectedOptions: string[], code: FilterType) => void;
 }
 
-export const MobileFilterItem: React.SFC<MobileFilterProps> = ({
+export const MobileAdvancedFilter: React.SFC<MobileFilterProps> = ({
   onMobileSelect,
   filterOptions,
   isMultiSelect,
