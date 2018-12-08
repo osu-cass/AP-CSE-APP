301be4d8ec9d8dce03b36e58010c3030e29336a5
Thomas Noelcke <noelcket@oregonstate.edu>
2018-12-08 15:49:31 -0800

    fix(mobile filter): add logic for subject update

--- src/components/MobileFilter/index.storybook.tsx
+++ src/components/MobileFilter/index.storybook.tsx
@@ -15,12 +15,34 @@ const reset = () => {
   console.log('Filter Reset');
 };
 
+const onSubjectUpdate = (code: FilterType, data?: FilterOptionModel) => {
+  // tslint:disable-next-line:no-console
+  console.log('Subject Chnaged', JSON.stringify(data));
+};
+
 storiesOf('Mobile Filter', module)
   .addDecorator(centered)
-  .add('default', () => <MobileFilter filters={filterModelGS} onUpdate={callback} reset={reset} />)
+  .add('default', () => (
+    <MobileFilter
+      filters={filterModelGS}
+      onUpdate={callback}
+      reset={reset}
+      onSubjectUpdate={onSubjectUpdate}
+    />
+  ))
   .add('with claims', () => (
-    <MobileFilter filters={filterModelGSC} onUpdate={callback} reset={reset} />
+    <MobileFilter
+      filters={filterModelGSC}
+      onUpdate={callback}
+      reset={reset}
+      onSubjectUpdate={onSubjectUpdate}
+    />
   ))
   .add('with targets', () => (
-    <MobileFilter filters={filterModelCSCT} onUpdate={callback} reset={reset} />
+    <MobileFilter
+      filters={filterModelCSCT}
+      onUpdate={callback}
+      reset={reset}
+      onSubjectUpdate={onSubjectUpdate}
+    />
   ));
