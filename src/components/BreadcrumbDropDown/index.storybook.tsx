7ad9c6bc2c23765c471973a8763bcc1aa281e546
Thomas <noelcket@oregonstate.edu>
2018-12-12 21:17:15 -0800

    test(storyshot): add storybook for bread crumb drop down

---
+++ src/components/BreadcrumbDropDown/index.storybook.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { BrowserRouter } from 'react-router-dom';
+import { BreadcrumbDropDownProps, BreadcrumbDropDown } from './BreadCrumbDropDown';
+import { Colors } from '../../constants';
+
+const background = {
+  backgroundImage: `linear-gradient(90deg, ${Colors.sbBlue}, ${Colors.sbBlueLighter})`
+};
+
+const breadcrumbDropDownProps: BreadcrumbDropDownProps = {
+  currentTarget: {
+    label: 'Current Target',
+    code: 'a.b.c.ct'
+  },
+  targets: [
+    {
+      label: 'My Target',
+      code: 'a.b.c.e'
+    },
+    {
+      label: 'Target Two',
+      code: 'a.b.c.t2'
+    },
+    {
+      label: 'Target Three',
+      code: 'a.b.c.t3'
+    }
+  ]
+};
+
+storiesOf('Breadcrumb Drop Down Menu', module)
+  .addDecorator(story => <BrowserRouter>{story()}</BrowserRouter>)
+  .add('Renders with no list', () => (
+    <div style={background}>
+      <BreadcrumbDropDown targets={[]} currentTarget={breadcrumbDropDownProps.currentTarget} />
+    </div>
+  ))
+  .add('Renders with list', () => (
+    <div style={background}>
+      <BreadcrumbDropDown
+        targets={breadcrumbDropDownProps.targets}
+        currentTarget={breadcrumbDropDownProps.currentTarget}
+      />
+    </div>
+  ));
