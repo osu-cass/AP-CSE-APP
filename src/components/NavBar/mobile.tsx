f3a2ea3fc4509deee1cd1bff68da63f99101ed60
Reese Benson <bensonre@oregonstate.edu>
2018-12-13 11:49:16 -0800

    refactor(mobile-nav-bar): fix scrollability

--- src/components/NavBar/mobile.tsx
+++ src/components/NavBar/mobile.tsx
@@ -11,57 +11,47 @@ const noUnderline: React.CSSProperties = {
 
 export const MobileNavBar: React.SFC = () => (
   <nav>
-    <div className="link-container">
-      <Link to="/" style={noUnderline}>
-        <div>
-          <Home {...mobileIconStyle} />
-          Home
-        </div>
-      </Link>
-      <Link to="search" style={noUnderline}>
-        <div>
-          <Search {...mobileIconStyle} />
-          Search
-        </div>
-      </Link>
-      <Link to="search" style={noUnderline}>
-        <div>
-          <Eye {...mobileIconStyle} />
-          Explore
-        </div>
-      </Link>
-      <Link to="apps" style={noUnderline}>
-        <div>
-          <Grid {...mobileIconStyle} />
-          Apps
-        </div>
-      </Link>
-    </div>
+    <Link to="/" style={noUnderline}>
+      <div>
+        <Home {...mobileIconStyle} />
+        Home
+      </div>
+    </Link>
+    <Link to="search" style={noUnderline}>
+      <div>
+        <Search {...mobileIconStyle} />
+        Search
+      </div>
+    </Link>
+    <Link to="search" style={noUnderline}>
+      <div>
+        <Eye {...mobileIconStyle} />
+        Explore
+      </div>
+    </Link>
+    <Link to="apps" style={noUnderline}>
+      <div>
+        <Grid {...mobileIconStyle} />
+        Apps
+      </div>
+    </Link>
     <style jsx>{`
       nav {
         position: fixed;
-        top: 0px;
-        height: 100%;
-        display: flex;
-        align-items: end;
-        flex-direction: column;
-        justify-content: flex-end;
-      }
-      .link-container {
+        bottom: 0;
         display: flex;
         flex-direction: row;
         justify-content: space-between;
-        height: 40px;
+        min-height: 40px;
         background-color: ${Colors.sbGrayLighter};
-        width: 95vw;
-        bottom: 0px;
-        padding: 3px 10px;
+        width: 100vw;
       }
       div {
         display: flex;
         flex-direction: column;
         align-items: center;
         font-size: 0.75em;
+        padding: 3px;
       }
     `}</style>
   </nav>
