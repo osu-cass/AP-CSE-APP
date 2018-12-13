14889ab7464a042ff30be930fef6a9ddeb1e44ea
Thomas Noelcke <noelcket@oregonstate.edu>
2018-12-13 10:32:37 -0800

    refactor(pdf): created task model component

---
+++ src/components/PDFLink/Document/DocumentModels.ts
@@ -0,0 +1,11 @@
+
+
+export interface DocumentStyles {
+    page: object;
+    flexContainer: object;
+    pageNumber: object;
+    numberContainer: object;
+    flexImage: object;
+    header: object;
+    border: object;
+}
--- src/components/PDFLink/Document/index.tsx
+++ src/components/PDFLink/Document/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import '../../../../node_modules/typeface-pt-serif/index.css';
-import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+import { Document, Page, Text, View} from '@react-pdf/renderer';
 // import { Colors, Styles } from '../../../constants';
 import { Head } from './Head';
 import { OneColumnLayout } from './OneColumnLayout';
@@ -11,95 +11,10 @@ import { Standards } from './Standards';
 import { DOK } from './DOK';
 import { ParagraphContent } from './paragraphContent';
 import { Evidence } from './Evidence';
-import { TaskModel } from './TaskModel';
-import { ITaskModel } from '../../../models/target';
+import {OverviewProps, PageMeta} from './DocumentModels';
+import {styles} from './styles';
 
-interface DocumentStyles {
-  page: object;
-  flexContainer: object;
-  pageNumber: object;
-  numberContainer: object;
-  flexImage: object;
-  header: object;
-  border: object;
-}
 
-const styles: DocumentStyles = StyleSheet.create({
-  page: {
-    backgroundColor: 'white',
-    padding: 10,
-    paddingBottom: 60,
-    paddingTop: 30
-  },
-  flexContainer: {
-    display: 'flex'
-  },
-  pageNumber: {
-    position: 'absolute',
-    fontSize: 12,
-    bottom: 30,
-    left: 0,
-    right: 0,
-    textAlign: 'center',
-    color: 'black'
-  },
-  numberContainer: {
-    height: '25px',
-    position: 'relative'
-  },
-  flexImage: {
-    display: 'flex',
-    alignItems: 'right',
-    paddingRight: '20px'
-  },
-  header: {
-    fontSize: 12
-  },
-  border: {
-    border: '1px solid red'
-  }
-}) as DocumentStyles;
-
-export interface PageProps {
-  verticalRuler: Boolean;
-}
-
-export interface OverviewProps {
-  claim: IClaim;
-}
-
-interface PageMeta {
-  pageNumber: number;
-  totalPages: number;
-}
-
-export interface TaskModelProps {
-  taskModels: ITaskModel[];
-  claim: IClaim;
-}
-
-const TaskModel1: React.SFC<TaskModelProps> = ({ taskModels, claim }) => {
-  const taskModelsJSX: JSX.Element[] = [];
-  taskModels.forEach((element: ITaskModel, idx: number) =>
-    taskModelsJSX.push(
-      <Page key={`${idx}`} style={styles.page}>
-        <View key={`${idx} - ${element.taskName}`} style={styles.flexContainer} wrap>
-          <Head key={`${element.taskName} - ${idx}`} text={claim.title} />
-          <View key={`${idx}-${idx}`} wrap>
-            <TaskModel key={`${idx}-${idx}-${idx}`} content={element} />
-          </View>
-        </View>
-        <Text
-          style={styles.pageNumber}
-          render={({ pageNumber, totalPages }: PageMeta) => `${pageNumber} / ${totalPages}`}
-          fixed
-        />
-      </Page>
-    )
-  );
-
-  return <>{taskModelsJSX}</>;
-};
 const array: string[] = ['Task Model 1', 'Task Model 2'];
 
 const Overview = ({ claim }: OverviewProps) => (
