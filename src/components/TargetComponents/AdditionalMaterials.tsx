62aa64a9c3e2bf6aea6318b43c317f0ac40eaffe
Horne <hornee@oregonstate.edu>
2018-12-13 15:44:54 -0800

    fix(additionalmaterials): adds links to addtl materials

--- src/components/TargetComponents/AdditionalMaterials.tsx
+++ src/components/TargetComponents/AdditionalMaterials.tsx
@@ -6,10 +6,25 @@ export interface AdditionalMaterialsProps {
 }
 
 export const AdditionalMaterials: React.SFC<AdditionalMaterialsProps> = ({ target }) => {
+  const grade = target.split('.')[1].split('G')[1];
+  const subject = target.split('.')[0] === 'E' ? 'ELA' : 'MATH';
+  const claim = target.split('.')[2].split('')[1];
+  const targ = target.split('.')[3].split('T')[1];
   const materials = [
-    'Sample Items, Scoring Guides and Rubrics',
-    'Annotated Student Work',
-    'Digital Library Resources'
+    <a
+      href={`https://sampleitems.smarterbalanced.org/BrowseItems?Claim=${subject}${claim}&Subject=${subject}&Grade=${grade}&Target=${targ}`}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      'Sample Items, Scoring Guides and Rubrics'
+    </a>,
+    <a
+      href="http://www.smarterbalanced.org/educators/the-digital-library/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      'Digital Library Resources'
+    </a>
   ];
   const materialsJsx = materials.map((m, i) => (
     <div key={i}>
