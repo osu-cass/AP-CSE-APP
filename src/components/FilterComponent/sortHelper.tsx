c58f65a8f88f072a99fa4ab638276f5e011982a4
Horne <hornee@oregonstate.edu>
2018-12-18 14:12:58 -0800

    refactor(sorthelper): improves target handling

--- src/components/FilterComponent/sortHelper.tsx
+++ src/components/FilterComponent/sortHelper.tsx
@@ -2,11 +2,9 @@ import { FilterOptionModel } from '@osu-cass/sb-components';
 
 export const sortHelper = (filter: FilterOptionModel[]) => {
   filter.sort((a, b) => {
-    let x: string | number = a.label.toLowerCase();
-    let y: string | number = b.label.toLowerCase();
-    if (a.label.includes('Target ')) {
-      ({ x, y } = handleTargetSort([a, b]));
-    }
+    let x: string | number;
+    let y: string | number;
+    ({ x, y } = handleTargetSort([a, b]));
     if (x < y) {
       return -1;
     }
@@ -18,20 +16,26 @@ export const sortHelper = (filter: FilterOptionModel[]) => {
   });
 };
 function handleTargetSort(filters: FilterOptionModel[]) {
-  const res: number[] = [];
-  filters.forEach(f =>
-    res.push(
-      parseFloat(
-        f.label
-          .split(':')[0]
-          .split('Target ')[1]
-          .replace('a', '.1')
-          .replace('b', '.2')
+  if (filters[0].label.includes('Target ')) {
+    const res: number[] = [];
+    filters.forEach(f =>
+      res.push(
+        parseFloat(
+          f.label
+            .split(':')[0]
+            .split('Target ')[1]
+            .replace('a', '.1')
+            .replace('b', '.2')
+        )
       )
-    )
-  );
-  const x = res[0];
-  const y = res[1];
+    );
+    const x = res[0];
+    const y = res[1];
+
+    return { x, y };
+  }
+  const x = filters[0].label.toLowerCase();
+  const y = filters[1].label.toLowerCase();
 
   return { x, y };
 }
