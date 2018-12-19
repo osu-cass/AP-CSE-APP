26572c61649ce10f24be6f9b8e45d16c687cd7e3
Thomas Noelcke <noelcket@oregonstate.edu>
2018-12-19 12:52:11 -0800

    WIP: working on adding examples to item specs

--- src/components/TargetComponents/TargetDetail.tsx
+++ src/components/TargetComponents/TargetDetail.tsx
@@ -8,6 +8,7 @@ import { OrderedList } from './Lists';
 import { Stems } from './Stems';
 import { AdditionalMaterials } from './AdditionalMaterials';
 import { DOK } from './DOK';
+import { ExamplesProps } from '../TaskModelExample';
 
 export interface TargetDetailProps {
   target: ITarget;
@@ -32,6 +33,7 @@ function stemSection(stem: IStem[]): ContentSection[] {
 
   return sections;
 }
+
 // tslint:disable: no-non-null-assertion
 function handleFractions(taskModels: ITaskModel[]) {
   const rex = /\$\\frac{(\w+)}{(\w+)}\$/g;
@@ -73,7 +75,16 @@ function taskModelSections(
     }
 
     if (tm.examples && isMath) {
-      const examples = parseExamples(tm.examples);
+      const exampleProps: ExamplesProps = {
+        examples: []
+      };
+      if (typeof tm.examples === 'string') {
+        exampleProps.examples.push({ label: 'Example 1', content: tm.examples });
+      } else {
+        tm.examples.map((e, index) => {
+          exampleProps.examples.push({ label: `Example ${index + 1}`, content: e });
+        });
+      }
     }
 
     return {
