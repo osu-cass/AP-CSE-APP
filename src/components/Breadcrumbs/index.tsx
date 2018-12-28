5e2e2a4063bce18395a52ded9d4fdf38cdc575b0
Graham Barber <grahammbarber@gmail.com>
2018-12-28 14:49:38 -0800

    refactor(breadcrumbs): rename dropdown file to follow convention

--- src/components/Breadcrumbs/index.tsx
+++ src/components/Breadcrumbs/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Home } from 'react-feather';
 import { Link } from 'react-router-dom';
 import { BreadcrumbLink } from '../BreadcrumbLink/BreadcrumbLink';
-import { BreadcrumbDropDown } from '../BreadcrumbDropDown/BreadCrumbDropDown';
+import { BreadcrumbDropDown } from '../BreadcrumbDropDown';
 import { Styles, Colors } from '../../constants/style';
 import { CSEFilterParams, CSESearchQuery } from '../../models/filter';
 import { stringify } from 'query-string';
