561f7592e4174e679c595c0ab2cddee3fc28441c
Ingyu Woo <iglenwoo@gmail.com>
2019-01-08 11:10:01 -0800

    fix(render-table): add parsing table logic

--- src/components/TargetDetails/parseUtils.tsx
+++ src/components/TargetDetails/parseUtils.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ITable, Table } from './Table';
 
 const splitByNewLine = (text: string | undefined) => {
   if (text) {
@@ -57,6 +58,120 @@ const applyParsers = (parsers: ((text: string) => string)[], text: string) => {
   return parsedText;
 };
 
+// remove the last elem each row in data stack since it has one more column
+const sanitizeDataStack = (dataStack: string[], columnCount: number) => {
+  const columnCountWithEmpty: number = columnCount + 1;
+  let rowCount = Math.floor(dataStack.length / columnCountWithEmpty);
+
+  const newDataArray: string[] = [];
+  for (let i = 0; dataStack.length; i++) {
+    const data = dataStack.pop();
+    if (i % columnCountWithEmpty === 0 && rowCount > 0) {
+      rowCount--;
+      continue;
+    }
+    if (data !== undefined) {
+      newDataArray.push(data);
+    }
+  }
+
+  // reverse array since it is a stack
+  return newDataArray.reverse();
+};
+
+const getTableStacks = (text: string) => {
+  const tableStacks: { headerStack: string[]; cleanDataStack: string[] }[] = [];
+  let headerStack: string[] = [];
+  let dataStack: string[] = [];
+  let columnCount: number = 0;
+  let tableFound: Boolean = false;
+  const parts = text.split('|');
+
+  for (let i = parts.length; i--; ) {
+    const dashs = parts[i].match(/\-+/);
+
+    if (dashs && dashs[0] && dashs[0].length === parts[i].length) {
+      if (!tableFound) {
+        tableFound = true;
+      }
+      columnCount++;
+      continue;
+    }
+
+    if (tableFound) {
+      headerStack.push(parts[i]);
+
+      if (columnCount === 1) {
+        const cleanDataStack: string[] = sanitizeDataStack(dataStack, headerStack.length);
+        tableStacks.push({ headerStack, cleanDataStack });
+        headerStack = [];
+        dataStack = [];
+
+        columnCount = 0;
+        tableFound = false;
+        continue;
+      }
+      columnCount--;
+    } else {
+      dataStack.push(parts[i]);
+    }
+  }
+
+  return tableStacks;
+};
+
+const renderTable = (tableStack: { headerStack: string[]; cleanDataStack: string[] }) => {
+  const table: ITable = { Headers: [], DataRows: [] };
+  table.Headers = tableStack.headerStack.reverse();
+
+  let curColCount: number = 0;
+  let curRowIndex: number = 0;
+  let curRowData: string[] = [];
+  // if the last row doesn't match the number of columns, render it out of the table
+  while (tableStack.cleanDataStack.length !== 0) {
+    // NOTE: Argument of type 'string | undefined' is not assignable to parameter of type 'string'
+    curRowData.push(tableStack.cleanDataStack.pop());
+    curColCount++;
+
+    if (curColCount === tableStack.headerStack.length) {
+      table.DataRows.push(curRowData);
+
+      curColCount = 0;
+      curRowIndex++;
+      curRowData = [];
+    }
+  }
+
+  let elementJsx = <Table table={table} />;
+
+  // NOTE: Operator '+=' cannot be applied to types 'Element' and 'Element[]'
+  elementJsx += curRowData.map((col, index) => {
+    return <React.Fragment key={index}>{col}</React.Fragment>;
+  });
+
+  return elementJsx;
+};
+
+export const parseTable = (text: string) => {
+  const parts = text.split(/(\|.+\|)/s);
+
+  if (parts.length === 1) return parts[0]; // no table syntax found
+
+  return parts.map((part, index) => {
+    if (index === 1) {
+      // array[1] is the table syntax
+      const tableStacks: { headerStack: string[]; cleanDataStack: string[] }[] = getTableStacks(
+        part
+      );
+      tableStacks.map((tableStack, index) => {
+        return renderTable(tableStack);
+      });
+    }
+
+    return <React.Fragment key={index}>{part}</React.Fragment>;
+  });
+};
+
 const parseSingleAsterisk = (text: string, underlined: boolean) => {
   const parts = text.split('*');
 
@@ -97,6 +212,10 @@ export const parseImageTags = (text: string): JSX.Element => {
 };
 
 export const parseContent = (text: string | undefined) => {
+  // NOTE: this function should be refactored in order to parse tables
+  // NOTE: Argument of type 'string | undefined' is not assignable to parameter of type 'string'
+  const parsedTextWithTables = parseTable(text);
+
   const lines = splitByNewLine(text);
 
   if (!lines) return;
