267d2395a3dab6ed99d226d99b8fdf9f19df7630
Dan Van Horn <dan.vanhorn17@gmail.com>
2019-01-16 12:04:26 -0800

    fix(footer): remove min-height, add height property

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -31,8 +31,7 @@ export const Footer: React.SFC = () => {
           background-color: #ebebeb;
           display: flex;
           flex-flow: row wrap;
-          height: 1px;
-          min-height: 50px;
+          height: 50px;
           align-content: center;
           align-items: center;
           justify-content: center;
