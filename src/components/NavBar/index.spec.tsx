6628a3c97fd46958820a502ae187ef5b60bc8178
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2019-01-16 15:27:35 -0800

    test(nav-bar): yay more type safety!

--- src/components/NavBar/index.spec.tsx
+++ src/components/NavBar/index.spec.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { NavBar } from '.';
+import { SearchBar } from '../SearchBar';
 
 describe('<NavBar />', () => {
   it('handles search clicked', () => {
     const wrapper = shallow(<NavBar />);
-    const searchCallback = wrapper.find('SearchBar').prop('onSearch') as (search: string) => void;
+    const searchCallback = wrapper.find(SearchBar).prop('onSearch') as (search: string) => void;
 
     const locMock = jest.fn();
     window.location.assign = locMock;
