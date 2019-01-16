58dd5ef36cd0ca8b6d00d355f6ed6cae1b2da7ec
Horne <hornee@oregonstate.edu>
2019-01-16 10:16:07 -0800

    chore(testing): updates unit testing

--- src/components/PDFLink/Document/Document.spec.tsx
+++ src/components/PDFLink/Document/Document.spec.tsx
@@ -7,7 +7,11 @@ import { elaDocumentFull } from './mocks/mockDocumentProps';
 describe('CustomDocument', () => {
   it(`renders an ELA target`, () => {
     const component = shallow(createDocument(elaDocumentFull));
-
+    const setPageCount = jest.fn();
+    // tslint:disable-next-line: no-unsafe-any no-any
+    const callBackComponent = shallow(createDocument(elaDocumentFull), setPageCount());
     expect(toJson(component)).toMatchSnapshot();
+    expect(callBackComponent).toMatchSnapshot();
+    expect(setPageCount).toBeCalledTimes(1);
   });
 });
