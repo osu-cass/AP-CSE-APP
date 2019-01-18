f2b42c5bf7127e275fbe9bd2630f5bd396d54856
Ingyu Woo <iglenwoo@gmail.com>
2019-01-18 11:58:40 -0800

    test(snapshots): update snapshots

--- src/components/TargetDetails/TargetDetail.story.tsx
+++ src/components/TargetDetails/TargetDetail.story.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { RouterDecorator } from '../../__decorators__';
 import centered from '@storybook/addon-centered';
-import { targetMock, tableMock } from './__mocks__';
+import { targetMock } from './__mocks__';
 import { TargetDetail } from '.';
 import ELAG3ClaimMock from '../../../mock_api_data/E.G3.C1';
 import ELAG3C2T4Mock from '../../../mock_api_data/E.G3.PT';
-import { Table } from './Table';
 
 storiesOf('Target Page', module)
   .addDecorator(RouterDecorator)
